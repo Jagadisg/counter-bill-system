f1cb08a460d7050c9ed177a18c85d591529d8808
jagadish <jagadish.nadar41@gmail.com>
2024-04-09 15:04:02 +0530

    build changes

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo " BUILD START"
-python3.9 -m ensurepip --upgrade
-python3.9 -m pip install --upgrade pip setuptools 
-python3.9 -m pip install django
-python3.9 -m pip install psycopg2-binary
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python3.10 -m ensurepip --upgrade
+python3.10 -m pip install --upgrade pip setuptools 
+python3.10 -m pip install django
+python3.10 -m pip install psycopg2-binary
+python3.10 -m pip install -r requirements.txt
+python3.10 manage.py collectstatic --noinput --clear
 echo " BUILD END"
